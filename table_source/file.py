c0ed25baa139401095107cbf49f0454bf4c052b6
GreyKnight <tinyplasticgreyknight@yahoo.com>
2015-12-13 00:51:31 +0000

    load raw csv, module name

--- table_source/file.py
+++ table_source/file.py
@@ -1,10 +1,19 @@
+import csv
 from table_source.datasource import DataSource
 
 class File(DataSource):
     def __init__(self, filename):
         DataSource.__init__(self)
         self.filename = filename
-        self.load()
+        self.loadraw()
+        self.interpret()
 
-    def load(self):
-        pass
+    def loadraw(self):
+        raw = []
+        with open(self.filename, "r") as h:
+            for row in csv.reader(h):
+                raw.append(row)
+        self.raw = raw
+
+    def interpret(self):
+        self.module_name = self.raw[0][0]
