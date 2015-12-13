f7434ad5c3266284f1e3d575d16c9770d0511a82
GreyKnight <tinyplasticgreyknight@yahoo.com>
2015-12-13 02:23:37 +0000

    add dice notation parsing

--- random_source/__init__.py
+++ random_source/__init__.py
@@ -1,4 +1,7 @@
 import random
+import re
+
+DPATTERN = re.compile("^(\\d+)?d(\\d+)([+-]\\d+)?(x(\\d+))$")
 
 class Seed:
     def __init__(self, n):
@@ -14,8 +17,15 @@ class Seed:
     def chance(self, percent_chance):
         return self.randint(0, 99) < percent_chance
 
-    def d(self, n):
-        return self.randint(1, n)
+    def dparse(self, dstr):
+        matches = DPATTERN.fullmatch(dstr)
+        return self.d(int(matches.group(2)), int(matches.group(1)), int(matches.group(3)), int(matches.group(5)))
+
+    def d(self, sides, num=1, mod=0, multiplier=1):
+        rolled = 0
+        for _ in range(num):
+            rolled += self.randint(1, sides)
+        return (rolled + mod) * multiplier
 
     def index(self, length=20):
         return self.d(length) - 1
